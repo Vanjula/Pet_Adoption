535e134e46feab56a7e00b1a72b7e00ed8c2efdf
ShyamsaranRajendran <sarancode6@gmail.com>
2024-11-11 23:28:33 +0530

    _________________________

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 
 module.exports={
-    // database: 'mongodb+srv://workspacemvsi:3fmr4tSsivTHor4I@cluster06.569xw8k.mongodb.net/pet'
-    database:'mongodb://localhost:27017/pet'
+    database: 'mongodb+srv://workspacemvsi:3fmr4tSsivTHor4I@cluster06.569xw8k.mongodb.net/pet'
+    // database:'mongodb://localhost:27017/pet'
 }
